cf54cdc7a6650e950ad731bf6ad4ac0200036c48
Andrey Ryzhkov <rizkoff@gmail.com>
2022-01-08 19:19:30 +0000

    no auto_ack; auto rebalance consumers on fail

--- 02/consume/worker.py
+++ 02/consume/worker.py
@@ -12,8 +12,9 @@ def main():
         print(" [x] Received %r" % body.decode())
         time.sleep(body.count(b'.'))
         print(" [x] Done")
+        ch.basic_ack(delivery_tag = method.delivery_tag)
 
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue='hello', on_message_callback=callback)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
