1ebd5c341054c6145772183d904f27605a9130db
Andrey Ryzhkov <rizkoff@gmail.com>
2022-01-08 19:50:22 +0000

    durable=True adds persistence to mq broker

--- 02/consume/worker.py
+++ 02/consume/worker.py
@@ -6,7 +6,7 @@ def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
     channel = connection.channel()
 
-    channel.queue_declare(queue='hello')
+    channel.queue_declare(queue='task_queue', durable=True)
 
     def callback(ch, method, properties, body):
         print(" [x] Received %r" % body.decode())
@@ -14,7 +14,7 @@ def main():
         print(" [x] Done")
         ch.basic_ack(delivery_tag = method.delivery_tag)
 
-    channel.basic_consume(queue='hello', on_message_callback=callback)
+    channel.basic_consume(queue='task_queue', on_message_callback=callback)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
--- 02/publish/new_task.py
+++ 02/publish/new_task.py
@@ -5,12 +5,15 @@ import sys
 connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
 channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='task_queue', durable=True)
 
 message = ' '.join(sys.argv[1:]) or "Hello World!"
 channel.basic_publish(exchange='',
-                      routing_key='hello',
-                      body=message)
+                      routing_key='task_queue',
+                      body=message,
+                      properties=pika.BasicProperties(
+                         delivery_mode = pika.spec.PERSISTENT_DELIVERY_MODE
+                      ))
 print(" [x] Sent %r" % message)
 
 connection.close()
