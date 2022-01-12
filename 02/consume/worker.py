807417e9ae1cbccc53271fe9b21dcc86f18598f7
Andrey Ryzhkov <rizkoff@gmail.com>
2022-01-12 16:14:23 +0000

    channel.basic_qos(prefetch_count=1)

--- 02/consume/worker.py
+++ 02/consume/worker.py
@@ -14,6 +14,7 @@ def main():
         print(" [x] Done")
         ch.basic_ack(delivery_tag = method.delivery_tag)
 
+    channel.basic_qos(prefetch_count=1)
     channel.basic_consume(queue='task_queue', on_message_callback=callback)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
